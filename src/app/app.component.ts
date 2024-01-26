701d1e77bc131c4d5b36dd2c336ff5707c0b42af
jiajunwang95 <jiajun.wang.2018@smu.edu.sg>
2024-01-26 15:18:23 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'infosys_v2';
+export class AppComponent implements OnInit{
+  MenuList = [
+    {
+      title : 'Overview',
+      icon : 'bar-chart',
+      open : false,
+      selected : true,
+      disabled : false,
+      path : '',
+    },    {
+      title : 'Request',
+      icon : 'medicine-box',
+      open : false,
+      selected : false,
+      disabled : false,
+      path : '',
+    }
+
+  ]
+  metricSchema = [{head : 'hard', col : [[{head : 'OS'},{head : 'DB'}],[{head : 'OS'},{head : 'DB'}]]},{head : 'patch', col : [{head : 'OS'},{head : 'DB'}]}]
+  ngOnInit():void{
+    console.log("Perform your magic here....")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//NG ZORRO
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+
+//App Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './components/dashboards/overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzMenuModule,
+    NzGridModule,
+    NzButtonModule,
+    NzIconModule,
+    NzListModule,
+    NzToolTipModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
     AppRoutingModule
   ],
   providers: [],
